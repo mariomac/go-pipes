7cc28cf9f484d5c9a843b7e417e73de7d1ed14a9
Mario Macias <mario.macias@grafana.com>
2024-11-29 10:59:49 +0100

    Moving basic_linear to measure-based tutorial

--- tutorials/under-the-hood/basic_linear/basic_linear.go
+++ tutorials/under-the-hood/basic_linear/basic_linear.go
@@ -2,76 +2,94 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/mariomac/pipes/pipe"
 )
 
-func ranger(from, to int) func(out chan<- int) {
-	return func(out chan<- int) {
-		for i := from; i < to; i++ {
-			out <- i
+const numSamples = 10
+
+type Measure struct {
+	Timestamp time.Time
+	Value     float64
+}
+
+// forwards fake/random measures every 100 milliseconds
+func measurer(numSamples int) func(out chan<- Measure) {
+	return func(out chan<- Measure) {
+		clock := time.NewTicker(100 * time.Millisecond)
+		for i := 0; i < numSamples; i++ {
+			out <- Measure{<-clock.C, rand.Float64()}
 		}
 	}
 }
 
-func evenFilter(in <-chan int, out chan<- int) {
-	for n := range in {
-		if n%2 != 0 {
-			out <- n
+// sums the values of the measures within the same second
+// and forwards them as a single added measure for that second
+func summarizer(in <-chan Measure, out chan<- Measure) {
+	currentSeconds := time.Now().Unix()
+	sum := float64(0)
+	for measure := range in {
+		if measure.Timestamp.Unix() != currentSeconds {
+			out <- Measure{time.Unix(currentSeconds, 0), sum}
+			currentSeconds++
+			sum = 0
 		}
+		sum += measure.Value
 	}
+	out <- Measure{time.Unix(currentSeconds, 0), sum}
 }
 
-func printer(in <-chan int) {
-	for n := range in {
-		fmt.Print(n, " ")
+func logger(in <-chan Measure) {
+	for measure := range in {
+		fmt.Println(measure.Timestamp, "->", measure.Value)
 	}
-	fmt.Println()
 }
 
 func runManualPipeline() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	ch1 := make(chan Measure)
+	ch2 := make(chan Measure)
 	go func() {
-		ranger(5, 15)(ch1)
+		measurer(numSamples)(ch1)
 		close(ch1)
 	}()
 	go func() {
-		evenFilter(ch1, ch2)
+		summarizer(ch1, ch2)
 		close(ch2)
 	}()
-	printer(ch2)
+	logger(ch2)
 }
 
 type Pipeline struct {
-	ranger  pipe.Start[int]
-	filter  pipe.Middle[int, int]
-	printer pipe.Final[int]
+	measurer   pipe.Start[Measure]
+	summarizer pipe.Middle[Measure, Measure]
+	logger     pipe.Final[Measure]
 }
 
-func (p *Pipeline) Ranger() *pipe.Start[int]       { return &p.ranger }
-func (p *Pipeline) Filter() *pipe.Middle[int, int] { return &p.filter }
-func (p *Pipeline) Printer() *pipe.Final[int]      { return &p.printer }
+func (p *Pipeline) Ranger() *pipe.Start[Measure]           { return &p.measurer }
+func (p *Pipeline) Filter() *pipe.Middle[Measure, Measure] { return &p.summarizer }
+func (p *Pipeline) Printer() *pipe.Final[Measure]          { return &p.logger }
 
 func (p *Pipeline) Connect() {
-	p.ranger.SendTo(p.filter)
-	p.filter.SendTo(p.printer)
+	p.measurer.SendTo(p.summarizer)
+	p.summarizer.SendTo(p.logger)
 }
 
 func runAutoPipe() {
 	// builder and register nodes
-	builder := pipe.NewBuilder(&Pipeline{}, pipe.ChannelBufferLen(1))
-	pipe.AddStart(builder, (*Pipeline).Ranger, ranger(5, 15))
-	pipe.AddMiddle(builder, (*Pipeline).Filter, evenFilter)
-	pipe.AddFinal(builder, (*Pipeline).Printer, printer)
+	builder := pipe.NewBuilder(&Pipeline{})
+	pipe.AddStart(builder, (*Pipeline).Ranger, measurer(numSamples))
+	pipe.AddMiddle(builder, (*Pipeline).Filter, summarizer)
+	pipe.AddFinal(builder, (*Pipeline).Printer, logger)
 	run, _ := builder.Build()
 	run.Start()
 	<-run.Done()
 }
 
 func main() {
-	fmt.Print("Manual: ")
+	fmt.Println("=== Manual ===")
 	runManualPipeline()
-	fmt.Print("Auto:   ")
+	fmt.Println("=== Auto ===")
 	runAutoPipe()
 }
